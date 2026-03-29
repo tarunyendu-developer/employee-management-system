8741493fb7023388a38c6244798291531711ef02
tarunyendu-developer <tarunyendu2807@gmail.com>
2026-03-29 20:07:53 +0530

    Added UserRepository and EmployeeRepository

---
+++ employee-management/src/main/java/com/stackly/employee_management/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.stackly.employee_management.repository;
+
+import com.stackly.employee_management.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    List<Employee> findByDepartment(String department);
+}
