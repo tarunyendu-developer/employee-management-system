e134b8b0ad0c7b4d88214429955d8235ac469164
tarunyendu-developer <tarunyendu2807@gmail.com>
2026-03-29 22:00:27 +0530

    Added role-based access control using PreAuthorize

--- employee-management/src/main/java/com/stackly/employee_management/controller/EmployeeController.java
+++ employee-management/src/main/java/com/stackly/employee_management/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.stackly.employee_management.controller;
 import com.stackly.employee_management.entity.Employee;
 import com.stackly.employee_management.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,6 +16,7 @@ public class EmployeeController {
     private EmployeeService service;
 
     // GET ALL
+    @PreAuthorize("hasAnyRole('ADMIN','EMPLOYEE')")
     @GetMapping
     public List<Employee> getAllEmployees() {
         return service.getAll();
@@ -27,12 +29,14 @@ public class EmployeeController {
     }
 
     // CREATE
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public Employee createEmployee(@RequestBody Employee employee) {
         return service.create(employee);
     }
 
     // UPDATE
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public Employee updateEmployee(@PathVariable Long id,
                                    @RequestBody Employee employee) {
@@ -40,6 +44,7 @@ public class EmployeeController {
     }
 
     // DELETE
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public String deleteEmployee(@PathVariable Long id) {
         service.delete(id);
@@ -51,4 +56,5 @@ public class EmployeeController {
     public List<Employee> searchByDepartment(@RequestParam String department) {
         return service.search(department);
     }
+
 }
--- employee-management/src/main/java/com/stackly/employee_management/security/JwtFilter.java
+++ employee-management/src/main/java/com/stackly/employee_management/security/JwtFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
@@ -26,6 +28,8 @@ public class JwtFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
 
         final String authHeader = request.getHeader("Authorization");
+
+        List<SimpleGrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_EMPLOYEE")); // defaul
 
         String username = null;
         String token = null;
@@ -42,11 +46,7 @@ public class JwtFilter extends OncePerRequestFilter {
             if (jwtUtil.validateToken(token, username)) {
 
                 UsernamePasswordAuthenticationToken authToken =
-                        new UsernamePasswordAuthenticationToken(
-                                username,
-                                null,
-                                Collections.emptyList()
-                        );
+                        new UsernamePasswordAuthenticationToken(username, null, authorities);
 
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
